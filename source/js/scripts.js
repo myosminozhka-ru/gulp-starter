e17f8c2188ac588983019c48724a8e539d2b0fc7
shesterina <shesterina@myosminozhka.ru>
2025-04-09 17:29:49 +0300

    add swiper init

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,6 +1,12 @@
-var swiper = new Swiper(".mySwiper", {
-    navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
+window.addEventListener('DOMContentLoaded', () => {
+  // Инициализация всей слайдеров на странице по дата атрибуту
+  const sliderSectionElems = document.querySelectorAll('[data-slider]');
+  if (sliderSectionElems.length > 0) {
+    sliderSectionElems.forEach(sliderSectionEl => {
+      const swiperWrapper = sliderSectionEl.closest('[data-slider-section]');
+      const swiperArrows = swiperWrapper.querySelector('[data-slider-arrows]');
+      const swiperCounter = swiperWrapper.querySelector('[data-slider-counter]') || null;
+      sliderInit(sliderSectionEl, swiperArrows, swiperCounter);
+    });
+  }
+})
--- source/js/utils.js
+++ source/js/utils.js
@@ -30,3 +30,134 @@ export function toggleClassHandler(element, otherElems, className = 'active') {
     element.classList.add(className);
   }
 }
+/**
+ * @event formatMoney
+ * @param sliderEl {HTMLElement}
+ * @param swiperArrows {HTMLElement}
+ * @param swiperCounter {HTMLElement}
+ * @description Слайдер универсальный.
+ * Пример добавления разных размеров data-slide-count='{"default":1.3, "sm":2.2, "md":2.5, "lg":3.2, "xl":4}' и отступов data-between='{"default":16, "lg":20}'
+ */
+window.sliderInit = function(sliderEl, swiperArrows, swiperCounter){
+  const swiperPrev = swiperArrows ? swiperArrows.querySelector('[data-slider-prev]') : null;
+  const swiperNext = swiperArrows ? swiperArrows.querySelector('[data-slider-next]') : null;
+  const swiperCountSlide = sliderEl.dataset.slideCount;
+  const swiperBetweenSlide = sliderEl.dataset.between;
+  //Точки пагинации или счетчик слайдов
+  let pagination = {
+    el: null,
+  };
+  if(swiperCounter) {
+    pagination = {
+      el: swiperCounter,
+      type: 'custom',
+      renderCustom: function (swiper, current, total) {
+        const position = 100 - (current / total * 100);
+        const progressBarHtml = `<div class="swiper-progressbar"><div class="swiper-progressbar__fill" style="transform: translateX(-${position}%)"></div></div>`;
+        const counterHtml = `<div class="swiper-counter">${current}/${total}</div>`
+
+        return progressBarHtml + counterHtml;
+      }
+    }
+  }
+
+  // Сбор количества слайдов и отступов для брейкпоинтов
+  let slidesPerView = 1;
+  let swiperBreakpoints = {};
+  let spaceBetween = 20;
+  const breakpointConfig = {
+    360: 'xs',
+    576: 'sm',
+    767: 'md',
+    992: 'lg',
+    1200: 'xl',
+    1443: 'xxl'
+  };
+  if (swiperCountSlide) {
+    try {
+      const breakpoints = JSON.parse(swiperCountSlide);
+      slidesPerView = breakpoints.default || 1; // Гарантируем slidesPerView имеет значение по умолчанию
+
+      for (const width in breakpointConfig) {
+        if (breakpointConfig.hasOwnProperty(width)) {
+          const breakpointKey = breakpointConfig[width];
+          let slidesPerViewValue = breakpoints[breakpointKey];
+
+          if (width === '360') {
+            slidesPerViewValue = breakpoints.xs || breakpoints.default;
+          }  else if (width === '1443') {
+            slidesPerViewValue = breakpoints.xxl || breakpoints.xl;
+          }
+
+
+          if (typeof slidesPerViewValue === 'number') {
+            swiperBreakpoints[width] = {
+              slidesPerView: slidesPerViewValue
+            };
+          }
+        }
+      }
+    } catch (error) {
+      console.error("Ошибка при разборе swiperCountSlide:", error);
+    }
+  }
+  if (swiperBetweenSlide) {
+    try {
+      const between = JSON.parse(swiperBetweenSlide);
+      spaceBetween = between.default || 20; // Гарантируем spaceBetween имеет значение по умолчанию
+
+      // Проходим по существующим точкам останова в swiperBreakpoints и добавляем spaceBetween
+      for (const width in swiperBreakpoints) {
+        if (swiperBreakpoints.hasOwnProperty(width)) {
+          let spaceBetweenValue;
+          if (width === '360') {
+            spaceBetweenValue = between.xs || between.default;
+          } else if (width === '1443'){
+            spaceBetweenValue = between.xxl || between.xl || between.default;
+          } else {
+            // Используем breakpointConfig для определения ключа
+            const breakpointKey = breakpointConfig[width];
+            spaceBetweenValue = between[breakpointKey] || between.default;
+
+
+          }
+
+
+          if (typeof spaceBetweenValue === 'number') {
+            swiperBreakpoints[width].spaceBetween = spaceBetweenValue;
+          }
+        }
+      }
+    } catch (error) {
+      console.error("Ошибка при разборе swiperBetweenSlide:", error);
+      // Здесь можно предусмотреть обработку ошибки, например, показать сообщение пользователю
+    }
+  }
+
+  // Инициализация слайдера
+  return new Swiper(sliderEl, {
+    slidesPerView,
+    spaceBetween,
+    watchSlidesProgress: true,
+    autoHeight: sliderEl.dataset.slider === 'auto-height' || false,
+    effect: sliderEl.dataset.slider === 'fade' ? "fade" : false,
+    lazy: {
+      enabled: true,
+      loadOnTransitionStart: true
+    },
+    pagination,
+    navigation: {
+      nextEl: swiperNext,
+      prevEl: swiperPrev
+    },
+    breakpoints: swiperBreakpoints,
+    on: {
+      lock: function () {
+        if(swiperArrows) swiperArrows.classList.add('hidden');
+      },
+      unlock: function(){
+        if(swiperArrows) swiperArrows.classList.remove('hidden')
+      }
+    },
+  });
+}
